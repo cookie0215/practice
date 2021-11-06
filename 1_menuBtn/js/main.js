dd4b77297a6f917d9227bbd0247e04d1cfbec90f
cookie0215 <jina_0215@naver.com>
2021-11-06 15:26:42 +0900

    [JS연습] 메뉴 버튼 : 스크립트로 메뉴 클릭 시  스타일 변경

--- 1_menuBtn/js/main.js
+++ 1_menuBtn/js/main.js
@@ -1,6 +1,21 @@
-const gnb = document.getElementById("#gnd");
+const gnb = document.querySelector("#gnb");
 const menu = gnb.querySelectorAll("li");
 
-menu[0].addEventListener('click',function(){
-    
-});
+menu.forEach((el, index) => {
+    el.addEventListener('click',(e) => {
+        e.preventDefault;
+        console.log("클릭");
+
+        // 클릭 안한 메뉴 버튼 비활성화
+        for(let menuList of menu){
+            menuList.style.borderBottom = "none";
+            menuList.querySelector("a").style.color = "#555";
+            menuList.querySelector("a").style.fontWeight = "normal";
+        }
+
+        // 클릭한 메뉴 버튼 활성화
+        el.style.borderBottom = "2px solid hotpink";
+        el.querySelector("a").style.color = "hotpink";
+        el.querySelector("a").style.fontWeight = "bold";
+    });
+})
