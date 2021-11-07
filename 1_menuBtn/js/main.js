98d1de5b1219d26fb08331862a09e7990a2f8c1b
cookie0215 <jina_0215@naver.com>
2021-11-07 13:26:26 +0900

    activeation함수 분리

--- 1_menuBtn/js/main.js
+++ 1_menuBtn/js/main.js
@@ -4,18 +4,20 @@ const menu = gnb.querySelectorAll("li");
 menu.forEach((el, index) => {
     el.addEventListener('click',(e) => {
         e.preventDefault;
-        console.log("클릭");
-
-        // 클릭 안한 메뉴 버튼 비활성화
-        for(let menuList of menu){
-            menuList.style.borderBottom = "none";
-            menuList.querySelector("a").style.color = "#555";
-            menuList.querySelector("a").style.fontWeight = "normal";
-        }
-
-        // 클릭한 메뉴 버튼 활성화
-        el.style.borderBottom = "2px solid hotpink";
-        el.querySelector("a").style.color = "hotpink";
-        el.querySelector("a").style.fontWeight = "bold";
+        activation(index);
     });
 })
+
+function activation(index){
+    // 클릭 안한 메뉴 버튼 비활성화
+    for(let menuList of menu){
+        menuList.style.borderBottom = "none";
+        menuList.querySelector("a").style.color = "#555";
+        menuList.querySelector("a").style.fontWeight = "normal";
+    }
+
+    // 클릭한 메뉴 버튼 활성화
+    menu[index].style.borderBottom = "4px solid orange";
+    menu[index].querySelector("a").style.color = "orange";
+    menu[index].querySelector("a").style.fontWeight = "bold";
+}
